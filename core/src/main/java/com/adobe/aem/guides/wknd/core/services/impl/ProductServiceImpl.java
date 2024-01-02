55f731bcc0e609705a240508e84d57f664996e6b
Yury Korzun <yury.korzun@ds.pl>
2024-01-02 12:57:39 +0100

    ProductService: OSGI configuration

--- core/src/main/java/com/adobe/aem/guides/wknd/core/services/impl/ProductServiceImpl.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/services/impl/ProductServiceImpl.java
@@ -5,27 +5,84 @@ import com.adobe.aem.guides.wknd.core.services.ProductService;
 import com.adobe.aem.guides.wknd.core.services.ProductsRequestParams;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import lombok.Getter;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.AttributeType;
+import org.osgi.service.metatype.annotations.Designate;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 import java.util.logging.Logger;
 
 @Component(service = ProductService.class)
+@Designate(ocd = ProductServiceImpl.ServiceConfig.class)
 public class ProductServiceImpl implements ProductService {
+
+    /*  properties definition  */
+
+    public final static String PROPERTY_NAME_SERVICE_NAME = "serviceName";
+    public final static String PROPERTY_NAME_API_URL = "apiUrl";
+    public final static String PROPERTY_NAME_UNUSED = "unused.property";
+
+    @ObjectClassDefinition(
+            name = "Product service OSGI configuration"
+        ,   description = "Product service OSGI configuration"
+    )
+    public @interface ServiceConfig {
+
+        @AttributeDefinition(
+                name = "Service name"
+            ,   description = "The name of the service"
+            ,   type = AttributeType.STRING
+        )
+        public String serviceName() default "Products service";
+
+        @AttributeDefinition(
+                name = "products API URL"
+            ,   description = "URL of products API"
+            ,   type = AttributeType.STRING
+        )
+        String apiUrl() default "https://fakestoreapi.com/products";
+
+        @AttributeDefinition(
+                name = "unused property"
+            ,   description = "Unused property with and ierarchic name to test property injection in the test class"
+            ,   type = AttributeType.INTEGER
+        )
+        int unused_property() default 1;
+    }
 
     private final Logger logger = Logger.getLogger(this.getClass().getName());
 
-    private static final String URL_API_PRODUCTS_LIST = "https://fakestoreapi.com/products";
+    /* actual logic */
+
+    //  the getters here are only to test config injection
+    @Getter
+    private String serviceName;
+    @Getter
+    private String URL_API_PRODUCTS_LIST;
+    @Getter
+    private int unusedProperty;
+
+    @Activate
+    protected void activate(ServiceConfig config) {
+        this.URL_API_PRODUCTS_LIST  = config.apiUrl();
+        this.serviceName            = config.serviceName();
+        this.unusedProperty         = config.unused_property();
+    }
 
     //  API doesn't provide the limit
     @Override
--- core/src/test/java/com/adobe/aem/guides/wknd/core/services/ProductServiceImplTest.java
+++ core/src/test/java/com/adobe/aem/guides/wknd/core/services/ProductServiceImplTest.java
@@ -2,18 +2,53 @@ package com.adobe.aem.guides.wknd.core.services;
 
 import com.adobe.aem.guides.wknd.core.entities.Product;
 import com.adobe.aem.guides.wknd.core.services.impl.ProductServiceImpl;
-import org.junit.jupiter.api.Test;
+import io.wcm.testing.mock.aem.junit5.AemContext;
+import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@ExtendWith({AemContextExtension.class, MockitoExtension.class})
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ProductServiceImplTest {
 
+    private final AemContext aemContext = new AemContext();
+    private final ProductServiceImpl service = new ProductServiceImpl();
+
+    private final String    DEFAULT_VALUE_SERVICE_NAME = "Product service under test";
+    private final String    DEFAULT_VALUE_API_URL      = "https://fakestoreapi.com/products";
+    private final int       DEFAULT_VALUE_UNUSED_PROPERTY = 1;
+
+    @BeforeEach
+    void setUp() {
+        Map<String, Object> params = new HashMap<>();
+        params.put(ProductServiceImpl.PROPERTY_NAME_API_URL,        DEFAULT_VALUE_API_URL);
+        params.put(ProductServiceImpl.PROPERTY_NAME_SERVICE_NAME,   DEFAULT_VALUE_SERVICE_NAME);
+        params.put(ProductServiceImpl.PROPERTY_NAME_UNUSED,         DEFAULT_VALUE_UNUSED_PROPERTY);
+        aemContext.registerInjectActivateService(service, params);
+    }
+
     @Test
+    @Order(1)
+    void configInjectionTest() {
+        System.out.println("running config injection test");
+        assertEquals(DEFAULT_VALUE_SERVICE_NAME,    service.getServiceName());
+        assertEquals(DEFAULT_VALUE_API_URL,         service.getURL_API_PRODUCTS_LIST());
+        assertEquals(DEFAULT_VALUE_UNUSED_PROPERTY, service.getUnusedProperty());
+    }
+
+
+    @Test
+    @Order(2)
     void getProductsTest() {
-        ProductServiceImpl service = new ProductServiceImpl();
+        System.out.println("running getProducts test");
         List<Product> emptyList = new ArrayList<>();
         assertEquals(emptyList, service.getProducts(ProductsRequestParams.builder().limit(0).build()));
 
