ea1f139434fda5cafb79ed80d2034765e50de943
Yury Korzun <yury.korzun@ds.pl>
2024-01-03 13:07:59 +0100

    js attempt

---
+++ ui.frontend/src/main/webpack/components/_productlist.js
@@ -0,0 +1,61 @@
+(function(){
+    "use strict";
+
+    var selectors = {
+        self:      '[data-cmp-is="products"]',
+        product:   '[data-cmp-is="product"]'
+    };
+
+    function ProductList(config) {
+
+        function init(config) {
+            config.element.removeAttribute("data-cmp-is");
+            console.log(config);
+        }
+
+        if (config && config.element) {
+            init(config);
+        }
+    }
+
+    // Best practice:
+    // Use a method like this mutation obeserver to also properly initialize the component
+    // when an author drops it onto the page or modified it with the dialog.
+    function onDocumentReady() {
+        var elements = document.querySelectorAll(selectors.self);
+        for (var i = 0; i < elements.length; i++) {
+            new ProductList({ element: elements[i] });
+        }
+
+        var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+        var body            = document.querySelector("body");
+        var observer         = new MutationObserver(function(mutations) {
+            mutations.forEach(function(mutation) {
+                // needed for IE
+                var nodesArray = [].slice.call(mutation.addedNodes);
+                if (nodesArray.length > 0) {
+                    nodesArray.forEach(function(addedNode) {
+                        if (addedNode.querySelectorAll) {
+                            var elementsArray = [].slice.call(addedNode.querySelectorAll(selectors.self));
+                            elementsArray.forEach(function(element) {
+                                new ProductList({ element: element });
+                            });
+                        }
+                    });
+                }
+            });
+        });
+
+        observer.observe(body, {
+            subtree: true,
+            childList: true,
+            characterData: true
+        });
+    }
+
+    if (document.readyState !== "loading") {
+        onDocumentReady();
+    } else {
+        document.addEventListener("DOMContentLoaded", onDocumentReady);
+    }
+}());
