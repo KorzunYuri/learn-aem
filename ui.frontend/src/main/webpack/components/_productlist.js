edce51ea7b59f69afcff4f0dec24284d063c2a16
Yury Korzun <yury.korzun@ds.pl>
2024-01-04 11:53:24 +0100

    reading component data from js

--- ui.frontend/src/main/webpack/components/_productlist.js
+++ ui.frontend/src/main/webpack/components/_productlist.js
@@ -2,60 +2,17 @@
     "use strict";
 
     var selectors = {
-        self:      '[data-cmp-is="products"]',
+        self:      '[data-cmp-is="productlist"]',
         product:   '[data-cmp-is="product"]'
     };
 
-    function ProductList(config) {
+    var productLists = document.querySelectorAll(selectors.self);
+    productLists.forEach(function (productList){
+        var productIds = Array.from(
+            productList.querySelectorAll(selectors.product),
+            (product) => product.attributes['data-id'].value
+        ).join(',');
+        console.log('products: ' + productIds);
+    });
 
-        function init(config) {
-            config.element.removeAttribute("data-cmp-is");
-            console.log(config);
-        }
-
-        if (config && config.element) {
-            init(config);
-        }
-    }
-
-    // Best practice:
-    // Use a method like this mutation obeserver to also properly initialize the component
-    // when an author drops it onto the page or modified it with the dialog.
-    function onDocumentReady() {
-        var elements = document.querySelectorAll(selectors.self);
-        for (var i = 0; i < elements.length; i++) {
-            new ProductList({ element: elements[i] });
-        }
-
-        var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
-        var body            = document.querySelector("body");
-        var observer         = new MutationObserver(function(mutations) {
-            mutations.forEach(function(mutation) {
-                // needed for IE
-                var nodesArray = [].slice.call(mutation.addedNodes);
-                if (nodesArray.length > 0) {
-                    nodesArray.forEach(function(addedNode) {
-                        if (addedNode.querySelectorAll) {
-                            var elementsArray = [].slice.call(addedNode.querySelectorAll(selectors.self));
-                            elementsArray.forEach(function(element) {
-                                new ProductList({ element: element });
-                            });
-                        }
-                    });
-                }
-            });
-        });
-
-        observer.observe(body, {
-            subtree: true,
-            childList: true,
-            characterData: true
-        });
-    }
-
-    if (document.readyState !== "loading") {
-        onDocumentReady();
-    } else {
-        document.addEventListener("DOMContentLoaded", onDocumentReady);
-    }
 }());
