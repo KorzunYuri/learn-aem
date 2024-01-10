aaab3c2a732e827fbcfa0a69ab9f4aa2f14cce02
Yury Korzun <yury.korzun@ds.pl>
2024-01-10 16:46:32 +0100

    Getting pageManager best practice

--- core/src/main/java/com/adobe/aem/guides/wknd/core/services/impl/ProductPagesGeneratorImpl.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/services/impl/ProductPagesGeneratorImpl.java
@@ -6,12 +6,11 @@ import com.adobe.aem.guides.wknd.core.services.ProductService;
 import com.adobe.aem.guides.wknd.core.services.ProductsRequestParams;
 import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.PageManager;
+import com.day.cq.wcm.api.PageManagerFactory;
 import com.day.cq.wcm.api.WCMException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
-import org.apache.sling.api.resource.LoginException;
-import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.apache.sling.api.resource.*;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Modified;
@@ -46,6 +45,9 @@ public class ProductPagesGeneratorImpl implements ProductPagesGenerator {
 
     @Reference
     private ResourceResolverFactory resourceResolverFactory;
+
+    @Reference
+    private PageManagerFactory pageManagerFactory;
 
 
     @ObjectClassDefinition(
@@ -109,7 +111,7 @@ public class ProductPagesGeneratorImpl implements ProductPagesGenerator {
         try (ResourceResolver resourceResolver = getResourceResolver()) {
             String userId = resourceResolver.getUserID();
             log.info("Got resourceResolver for user " + userId);
-            PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
+            PageManager pageManager = pageManagerFactory.getPageManager(resourceResolver);
 
             //  create new pages
             products.forEach(product -> {
@@ -154,7 +156,8 @@ public class ProductPagesGeneratorImpl implements ProductPagesGenerator {
         boolean isPagePropertiesUpdated = false;
         if (page != null) {
             try {
-                setPageContent(page, product, resourceResolver);
+                setPageContentByResourceApi(page, product, resourceResolver);
+                resourceResolver.commit();
                 isPagePropertiesUpdated = true;
             } catch (Exception e) {
                 log.error(String.format("Failed to populate page %s with title %s from template %s : %s", pagePath, pageTitle, templatePath, e.getMessage()));
@@ -171,7 +174,7 @@ public class ProductPagesGeneratorImpl implements ProductPagesGenerator {
         return page;
     }
 
-    private void setPageContent(Page page, Product product, ResourceResolver resolver) throws Exception {
+    private void setPageContentByNodeApi(Page page, Product product, ResourceResolver resolver) throws Exception {
         Node pageNode = page.adaptTo(Node.class);
         Node jcrNode = null;
         //  get/create jcr:content node
@@ -192,7 +195,28 @@ public class ProductPagesGeneratorImpl implements ProductPagesGenerator {
         productNode.setProperty("title", product.getTitle());
         productNode.setProperty("description", product.getDescription());
         productNode.setProperty("image", product.getImage());
-        resolver.commit();
+    }
+
+    private void setPageContentByResourceApi(Page page, Product product, ResourceResolver resolver) throws Exception {
+        //  get/create content resource
+        Resource pageResource = page.adaptTo(Resource.class);
+        Resource pageContentResource = page.getContentResource();
+        if (pageResource == null) {
+            Map<String, Object> contentProps = new HashMap<>();
+            contentProps.put("sling:primaryType", "cq:PageContent");
+            pageContentResource = resolver.create(pageResource, "jcr:content", contentProps);
+        }
+        //  get/create product resource
+        Resource productResource = pageContentResource.getChild("product");
+        if (productResource == null) {
+            productResource = resolver.create(pageContentResource, "product", new HashMap<>());
+        }
+        //  populate with properties
+        ModifiableValueMap productProperties = productResource.adaptTo(ModifiableValueMap.class);
+        productProperties.put("id", product.getId());
+        productProperties.put("title", product.getTitle());
+        productProperties.put("description", product.getDescription());
+        productProperties.put("image", product.getImage());
     }
 
     private String getPagePath(Product product) {
