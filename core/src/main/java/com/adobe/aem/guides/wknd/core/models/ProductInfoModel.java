89c792f9afb1a67493469f67a026149f0d5fab8b
Yury Korzun <yury.korzun@ds.pl>
2024-01-11 13:56:53 +0100

    Custom component ProductInfo: asset info: properties list

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/ProductInfoModel.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/ProductInfoModel.java
@@ -1,5 +1,7 @@
 package com.adobe.aem.guides.wknd.core.models;
 
+import java.util.List;
+
 public interface ProductInfoModel {
-    String getAssetInfo();
+    List<String> getAssetProperties();
 }
--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/ProductInfoModelImpl.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/ProductInfoModelImpl.java
@@ -9,12 +9,15 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.Self;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
 
 @Model(
         adaptables                  = {SlingHttpServletRequest.class}
@@ -39,8 +42,8 @@ public class ProductInfoModelImpl implements ProductInfoModel {
     }
 
     @Override
-    public String getAssetInfo() {
-        String assetInfo = "no asset info";
+    public List<String> getAssetProperties() {
+        List<String> propertyInfoList = new ArrayList<>();
         if (!StringUtils.isBlank(imageUri)) {
             Asset asset = null;
             try {
@@ -50,21 +53,21 @@ public class ProductInfoModelImpl implements ProductInfoModel {
                     asset = resource.adaptTo(Asset.class);
                 }
                 if (asset != null) {
-                    AssetMetadata assetMetadata = asset.getAssetMetadata();
-                    XMPMetadata xmp = assetMetadata.getXMP();
-                    StringBuilder sb = new StringBuilder();
-                    sb.append(xmp.getAboutURI()).append("\n");
-                    xmp.spliterator().forEachRemaining(xmpNode -> {
-                        sb.append(xmpNode.getName()).append("\n");
-                    });
-                    assetInfo = sb.toString();
-                    log.info(String.format("asset info: %s", assetInfo));
+                    populateWithProperties(propertyInfoList, asset.adaptTo(ValueMap.class));
+//                    Resource contentResource = resource.getChild("jcr:content");
+//                    populateWithProperties(propertyInfoList, contentResource.adaptTo(ValueMap.class));
+//                    Resource metadataResource = contentResource.getChild("metadata");
+//                    populateWithProperties(propertyInfoList, metadataResource.adaptTo(ValueMap.class));
                 }
             } catch (Exception e) {
                 log.error(String.format("Exception occured while trying to retrieve asset by path %s", imageUri));
             }
         }
-        return assetInfo;
+        return propertyInfoList;
+    }
+
+    private void populateWithProperties(List<String> propertiesList, ValueMap valueMap) {
+        valueMap.forEach((k,v) -> propertiesList.add(String.format("%s: %s", k, v)));
     }
 
 }
