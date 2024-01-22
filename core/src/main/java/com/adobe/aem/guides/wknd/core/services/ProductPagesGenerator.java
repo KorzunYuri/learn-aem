008d6ee0753fbcbd1a104ad16ea74a622d9e3b80
Yury Korzun <yury.korzun@ds.pl>
2024-01-22 16:42:00 +0100

    ProductPagesGenerator.getPagesLinks with RequestParams object

--- core/src/main/java/com/adobe/aem/guides/wknd/core/services/ProductPagesGenerator.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/services/ProductPagesGenerator.java
@@ -1,6 +1,10 @@
 package com.adobe.aem.guides.wknd.core.services;
 
+import java.util.List;
+
 public interface ProductPagesGenerator {
     void updateProductPages();
     String getPagesRoot();
+    List<String> getPagesLinks();
+    List<String> getPagesLinks(ProductPagesListRequest request);
 }
